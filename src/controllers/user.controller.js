bb1ea5e6bea990d6c5b7aaaea795cb87bda608a1
Vicky Kumar Kushwaha <vickykumarkusgwaha@gmail.com>
2025-07-19 10:32:19 +0530

    common changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import httpStatus from "http-status";
 import { User } from "../models/user.model.js";
-import bcrypt, { hash } from "bcrypt"
+import bcrypt, { hash } from "bcryptjs"
 
 import crypto from "crypto"
 import { Meeting } from "../models/meeting.model.js";
